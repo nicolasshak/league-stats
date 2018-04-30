50fd0a763ab41f4d9a6e7635759e551509c875b5
Nicolas Shak <Nshak@Ucsc.edu>
2018-04-30 14:38:54 -0700

    added -v to parser argument parser

--- apiTest.py
+++ apiTest.py
@@ -18,63 +18,71 @@ def isValidMatch(args, match):
 			return True
 	return False
 
+def main():
+	if len(sys.argv) == 1:
+		print('Missing summoner name. Correct usage is: python apiTest.py [SummonerName]')
+		exit()
 
-if len(sys.argv) == 1:
-	print('Missing summoner name. Correct usage is: python apiTest.py [SummonerName]')
-	exit()
+	parser = argparse.ArgumentParser(description = 'calculate winrate')
+	parser.add_argument('summonerName', help='summoner name for lookup', type = str)
+	parser.add_argument('-A', dest = 'getAllMatches', help = 'get all available matches', action = 'store_true')
+	parser.add_argument('-r', dest = 'rankedOnly', help = 'filter to ranked games only', action = 'store_true')
+	parser.add_argument('-c', dest = 'byChampion', nargs = 1, help = 'filter by champion', action = 'store')
+	parser.add_argument('-v', dest = 'verbose', help = 'segregate results by champion', action = 'store_true')
 
-parser = argparse.ArgumentParser(description = 'calculate winrate')
-parser.add_argument('summonerName', help='summoner name for lookup', type = str)
-parser.add_argument('-A', dest = 'getAllMatches', help = 'get all available matches', action = 'store_true')
-parser.add_argument('-r', dest = 'rankedOnly', help = 'filter to ranked games only', action = 'store_true')
-parser.add_argument('-c', dest = 'byChampion', nargs = 1, help = 'filter by champion', action = 'store')
+	args = parser.parse_args()
 
-args = parser.parse_args()
+	Riot = RiotWatcher(getKey())
+	champions = json.load(open(CHAMPIONS_PATH))
+	queues = json.load(open(QUEUES_PATH))
 
-Riot = RiotWatcher(getKey())
-champions = json.load(open(CHAMPIONS_PATH))
-queues = json.load(open(QUEUES_PATH))
+	summoner = Riot.summoner.by_name(REGION, sys.argv[1])
+	print summoner['name']
 
-summoner = Riot.summoner.by_name(REGION, sys.argv[1])
-print summoner['name']
+	if args.getAllMatches:
+		matches = Riot.match.matchlist_by_account(REGION, summoner['accountId'])
+	else:
+		matches = Riot.match.matchlist_by_account_recent(REGION, summoner['accountId'])
 
-if args.getAllMatches:
-	matches = Riot.match.matchlist_by_account(REGION, summoner['accountId'])
-else:
-	matches = Riot.match.matchlist_by_account_recent(REGION, summoner['accountId'])
+	games = [['Lane', 'Champion', 'Queue', 'Win']]
+	winrate = [['Wins', 'Losses', 'Winrate']]
+	wins = 0;
+	losses = 0;
 
-games = [['Lane', 'Champion', 'Queue', 'Win']]
-winrate = [['Wins', 'Losses', 'Winrate']]
-wins = 0;
-losses = 0;
+	for match in matches['matches']:
+		if isValidMatch(args, match):
 
-for match in matches['matches']:
-	if isValidMatch(args, match):
+			matchDto = Riot.match.by_id(REGION, match['gameId'])
+			participantId = -1
+			for participant in matchDto['participantIdentities']:
+				if participant['player']['summonerName'] == summoner['name']:
+					participantId = participant['participantId']
+			win = False
+			for participant in matchDto['participants']:
+				if participant['participantId'] == participantId:
+					win =  participant['stats']['win']
 
-		matchDto = Riot.match.by_id(REGION, match['gameId'])
-		participantId = -1
-		for participant in matchDto['participantIdentities']:
-			if participant['player']['summonerName'] == summoner['name']:
-				participantId = participant['participantId']
-		win = False
-		for participant in matchDto['participants']:
-			if participant['participantId'] == participantId:
-				win =  participant['stats']['win']
+			if win:
+				wins = wins + 1
+			else:
+				losses = losses + 1
 
-		if win:
-			wins = wins + 1
-		else:
-			losses = losses + 1
+			games.append([
+				match['lane'], 
+				champions[str(match['champion'])], 
+				queues[str(match['queue'])],
+				str(win)
+				])
 
-		games.append([
-			match['lane'], 
-			champions[str(match['champion'])], 
-			queues[str(match['queue'])],
-			str(win)
-			])
+	table_games = AsciiTable(games)
+	print table_games.table
 
-table = AsciiTable(games)
-print table.table
-winrate.append([str(wins), str(losses), float(wins)/float(wins + losses)])
-table2 = AsciiTable(winrate)
-print table2.table
+	if args.verbose:
+		print();
+
+	winrate.append([str(wins), str(losses), float(wins)/float(wins + losses)])
+	table_winrate = AsciiTable(winrate)
+	print table_winrate.table
+
+if __name__ == '__main__':
+	main()
