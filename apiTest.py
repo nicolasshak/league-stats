18d7092f02ee6f4e1dc6f87214c799654fa0fbc9
Nicolas Shak <Nshak@Ucsc.edu>
2018-04-30 14:41:53 -0700

    Forgot to remove resolution code

--- apiTest.py
+++ apiTest.py
@@ -18,7 +18,6 @@ def isValidMatch(args, match):
 			return True
 	return False
 
-<<<<<<< HEAD
 def main():
 	if len(sys.argv) == 1:
 		print('Missing summoner name. Correct usage is: python apiTest.py [SummonerName]')
@@ -88,72 +87,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-=======
-
-if len(sys.argv) == 1:
-	print('Missing summoner name. Correct usage is: python apiTest.py [SummonerName]')
-	exit()
-
-parser = argparse.ArgumentParser(description = 'calculate winrate')
-parser.add_argument('summonerName', help='summoner name for lookup', type = str)
-parser.add_argument('-A', dest = 'getAllMatches', help = 'get all available matches', action = 'store_true')
-parser.add_argument('-r', dest = 'byRanked', help = 'filter to ranked games only', action = 'store_true')
-parser.add_argument('-c', dest = 'byChampion', nargs = 1, help = 'filter by champion', action = 'store')
-parser.add_argument('-q', dest = 'byQueue', help = 'filter by queue', action = 'store_true')
-
-args = parser.parse_args()
-
-Riot = RiotWatcher(getKey())
-champions = json.load(open(CHAMPIONS_PATH))
-queues = json.load(open(QUEUES_PATH))
-
-summoner = Riot.summoner.by_name(REGION, sys.argv[1])
-print(summoner['name'])
-
-if args.getAllMatches:
-	matches = Riot.match.matchlist_by_account(REGION, summoner['accountId'])
-else:
-	matches = Riot.match.matchlist_by_account_recent(REGION, summoner['accountId'])
-
-games = [['Lane', 'Champion', 'Queue', 'Win']]
-winrate = [['Wins', 'Losses', 'Winrate']]
-wins = 0;
-losses = 0;
-
-for match in matches['matches']:
-	if isValidMatch(args, match):
-
-		matchDto = Riot.match.by_id(REGION, match['gameId'])
-		participantId = -1
-		for participant in matchDto['participantIdentities']:
-			if participant['player']['summonerName'] == summoner['name']:
-				participantId = participant['participantId']
-		win = False
-		for participant in matchDto['participants']:
-			if participant['participantId'] == participantId:
-				win =  participant['stats']['win']
-
-		if win:
-			wins = wins + 1
-		else:
-			losses = losses + 1
-
-		games.append([
-			match['lane'], 
-			champions[str(match['champion'])], 
-			queues[str(match['queue'])],
-			str(win)
-			])
-
-table = AsciiTable(games)
-print(table.table)
-
-if wins + losses == 0:
-	percentage = 'UNDEF'
-else:
-	percentage = float(wins + losses)
-
-winrate.append([str(wins), str(losses), percentage])
-table2 = AsciiTable(winrate)
-print(table2.table)
->>>>>>> d488261244680755c207e0e09e3e295b790f1cdc
